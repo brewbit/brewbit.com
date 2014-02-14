89cf19372d16a012a8371c6281711209b14f6a79
Nick Hebner <hebnern@gmail.com>
2014-02-14 00:03:40 -0800

    Base rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BrewbitSpree::Application.config.secret_key_base = 'd173665ec27006a4d21286be99d29622e831828787646856e50a221c218da0ec95b4984d2bf771c73acecc3622ba2db350d9d117a0fbae063f1876aa5f1b95ae'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BrewbitSpree::Application.config.session_store :cookie_store, key: '_brewbit-spree_session'
