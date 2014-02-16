5c739db72d17d806f1c75b77f06c94c7efae1a77
Nick Hebner <hebnern@gmail.com>
2014-02-15 21:21:31 -0800

    Fix warnings.

--- config/application.rb
+++ config/application.rb
@@ -32,5 +32,7 @@ module BrewbitSpree
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    
+    I18n.enforce_available_locales = false
   end
 end
