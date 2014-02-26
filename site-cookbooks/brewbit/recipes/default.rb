8efe18ec98f1f5c5c29c3d8de451a2fa7fce532f
Nick Hebner <hebnern@gmail.com>
2014-02-25 19:43:25 -0800

    Install nginx site-config to sites-available and symlink to sites-enabled.

--- site-cookbooks/brewbit/recipes/default.rb
+++ site-cookbooks/brewbit/recipes/default.rb
@@ -48,7 +48,11 @@ file "/etc/ssl/private/brewbit.com.key" do
   content ssl['key']
 end
 
-cookbook_file "nginx__sites-enabled__brewbit.com" do
-  path "/etc/nginx/sites-enabled/brewbit.com"
+cookbook_file "nginx__sites-available__brewbit.com" do
+  path "/etc/nginx/sites-available/brewbit.com"
   action [ :delete, :create ]
+end
+
+link "/etc/nginx/sites-enabled/brewbit.com" do
+  to "/etc/nginx/sites-available/brewbit.com"
 end
