3ac8022a3362395837aa7f4c5c83df5fe0d51556
Nick Hebner <hebnern@gmail.com>
2014-03-23 10:13:29 -0700

    Dynamic Setpoint -> Temp Profile

---
+++ db/migrate/20140323171156_rename_dynamic_setpoints_to_temp_profiles.spree_brewbit_dashboard.rb
@@ -0,0 +1,16 @@
+# This migration comes from spree_brewbit_dashboard (originally 20140323170636)
+class RenameDynamicSetpointsToTempProfiles < ActiveRecord::Migration
+  def up
+    rename_column :sensors, :dynamic_setpoint_id, :temp_profile_id
+    rename_column :dynamic_setpoint_steps, :dynamic_setpoint_id, :temp_profile_id
+    rename_table :dynamic_setpoint_steps, :temp_profile_steps
+    rename_table :dynamic_setpoints, :temp_profiles
+  end
+
+  def down
+    rename_column :sensors, :temp_profile_id, :dynamic_setpoint_id
+    rename_column :temp_profile_steps, :temp_profile_id, :dynamic_setpoint_id
+    rename_table :temp_profile_steps, :dynamic_setpoint_steps
+    rename_table :temp_profiles, :dynamic_setpoints
+  end
+end
