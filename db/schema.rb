9904b9a668062841efe150a9c74913c49315bcf4
Nick Hebner <hebnern@gmail.com>
2014-04-06 12:39:45 -0700

    Add setpoint to temp graphs.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140402042642) do
+ActiveRecord::Schema.define(version: 20140406192113) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -87,6 +87,7 @@ ActiveRecord::Schema.define(version: 20140402042642) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "sensor_settings_id"
+    t.float    "setpoint"
   end
 
   add_index "sensor_readings", ["sensor_id"], name: "index_sensor_readings_on_sensor_id", using: :btree
