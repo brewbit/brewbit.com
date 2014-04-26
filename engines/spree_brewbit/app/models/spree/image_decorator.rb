cef0bd3c6cab0d1bd4f35b279e22f4f76864b75b
Nick Hebner <hebnern@gmail.com>
2014-04-25 19:37:49 -0700

    Remove image size overrides.

--- engines/spree_brewbit/app/models/spree/image_decorator.rb
+++
@@ -1,8 +0,0 @@
-Spree::Image.class_eval do
-  attachment_definitions[:attachment][:styles] = {
-    :mini => '60x48#', # thumbs under image
-    :small => '160x122#', # images on category view
-    :product => '340x246#', # full product image
-    :large => '600x600#' # light box image
-  }
-end
